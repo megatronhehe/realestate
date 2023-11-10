5796a7bd6bf5d6a59b16461076a847d92e90a510
Megatronhehee <satyamahendra09@gmail.com>
2023-11-10 21:52:27 +0700

    fix: new hero design

--- src/pages/Main/Hero.jsx
+++ src/pages/Main/Hero.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 
 import heroImage from "../../assets/heroImage.jpg";
+import balikpapan from "../../assets/balikpapan.jpg";
+import surabaya from "../../assets/surabaya.jpg";
+import malang from "../../assets/malang.jpg";
+import bontang from "../../assets/bontang.jpg";
+import bali from "../../assets/bali.jpg";
 
 import { FaInstagram, FaWhatsapp, FaTiktok } from "react-icons/fa6";
 
@@ -8,43 +13,92 @@ import { Link } from "react-router-dom";
 
 const Hero = () => {
 	return (
-		<header className="h-screen text-gray-100 sm:h-96">
-			<div className="relative h-full ">
-				<img
-					src={heroImage}
-					alt="Background Image"
-					className="absolute top-0 left-0 object-cover w-full h-full"
-				/>
-				<div className="absolute top-0 left-0 flex flex-col items-center justify-center w-full h-full gap-8 p-6 duration-200 bg-black bg-opacity-70 hover:bg-opacity-50">
-					<h1 className="text-4xl">
-						<span className="font-semibold">Invest</span> in your{" "}
-						<span className="font-semibold">Tomorrow</span>
-					</h1>
-					<p>Affordable Personal Properties Options Today!</p>
-					<p className="text-center sm:w-2/3">
-						Here you can discover our humble listing of property such as houses
-						and lands spread across Indonesia
-					</p>
-					<Link
-						to="/properties"
-						className="px-5 py-2 text-sm font-semibold duration-200 bg-orange-400 rounded-full hover:scale-110"
-					>
-						our listing
-					</Link>
-				</div>
+		<header className="flex items-center justify-center h-screen text-gray-100">
+			<div className="w-full max-w-4xl p-6 ">
+				<div className="flex gap-8 sm:items-center">
+					<div className="text-gray-800  sm:static">
+						<h1 className="text-4xl">
+							<span className="font-semibold">Invest</span> in your{" "}
+							<span className="font-semibold text-orange-400">Tomorrow</span>
+						</h1>
 
-				<ul className="absolute flex gap-4 text-3xl text-gray-400 bottom-4 right-4">
-					<li className="duration-200 hover:text-white hover:scale-110">
-						<FaInstagram />
-					</li>
-					<li className="duration-200 hover:text-white hover:scale-110">
-						<FaWhatsapp />
-					</li>
-					<li className="duration-200 hover:text-white hover:scale-110">
-						<FaTiktok />
-					</li>
-				</ul>
+						<p className="mt-12">
+							Affordable Personal Properties Options Today, Here you can
+							discover our humble listing of property scuh as houses and lands
+							spread across Indonesia
+						</p>
+
+						<button className="px-3 py-1 mt-12 text-white bg-orange-400 rounded-xl">
+							Check our listing here
+						</button>
+					</div>
+
+					<div className="flex items-center gap-3">
+						<div className="flex flex-col w-1/3 gap-3">
+							<div className="w-full h-44 ">
+								<img
+									src={heroImage}
+									alt="hero-image"
+									className="object-cover h-full rounded-xl"
+								/>
+							</div>
+							<div className="w-full h-44 ">
+								<img
+									src={balikpapan}
+									alt="hero-image"
+									className="object-cover h-full rounded-xl"
+								/>
+							</div>
+						</div>
+						<div className="flex flex-col w-1/3 gap-3">
+							<div className="w-full h-44 ">
+								<img
+									src={surabaya}
+									alt="hero-image"
+									className="object-cover h-full rounded-xl"
+								/>
+							</div>
+							<div className="w-full h-44 ">
+								<img
+									src={malang}
+									alt="hero-image"
+									className="object-cover h-full rounded-xl"
+								/>
+							</div>
+							<div className="w-full h-44 ">
+								<img
+									src={bontang}
+									alt="hero-image"
+									className="object-cover h-full rounded-xl"
+								/>
+							</div>
+						</div>
+						<div className="flex flex-col w-1/3 gap-3">
+							<div className="w-full h-44 ">
+								<img
+									src={bali}
+									alt="hero-image"
+									className="object-cover h-full rounded-xl"
+								/>
+							</div>
+							<div className="w-full h-44 ">
+								<img
+									src={heroImage}
+									alt="hero-image"
+									className="object-cover h-full rounded-xl"
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
+
+			{/* <div className="absolute flex justify-center w-full h-full text-gray-700 duration-200 group hover:backdrop-blur-sm">
+				<h1 className="mt-12 text-2xl duration-500 group-hover:mt-40 group-hover:text-3xl group-hover:text-white">
+					Invest in your{" "}
+					<span className="font-semibold text-orange-400">Tomorrow</span>
+				</h1>
+			</div> */}
 		</header>
 	);
 };
