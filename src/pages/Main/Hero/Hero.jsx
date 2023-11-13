f4213f9124ed1500c0d8bbe289a53eab8f35e3e3
Megatronhehee <satyamahendra09@gmail.com>
2023-11-13 18:15:30 +0700

    feature added: contact us button in hero section

--- src/pages/Main/Hero/Hero.jsx
+++ src/pages/Main/Hero/Hero.jsx
@@ -1,40 +1,14 @@
 import React from "react";
 import HeroImages from "./HeroImages";
 
-import { Link } from "react-router-dom";
-
-import { FaArrowRightLong, FaDiamond } from "react-icons/fa6";
+import HeroText from "./HeroText";
 
 const Hero = () => {
 	return (
 		<header className="flex items-center justify-center h-screen text-gray-100">
 			<div className="w-full max-w-6xl p-0 sm:-mt-20 sm:p-6">
 				<div className="flex items-center gap-8 sm:relative ">
-					<div className="absolute z-10 flex flex-col justify-center h-full gap-8 p-6 text-white bg-black sm:justify-normal sm:bg-white sm:w-3/5 sm:text-gray-600 backdrop-blur-sm sm:static bg-opacity-40">
-						<FaDiamond className="text-4xl text-orange-400 animate-pulse" />
-						<h1 className="pb-8 text-4xl border-b">
-							<span className="font-semibold">Invest</span> in your{" "}
-							<span className="font-semibold text-orange-400">Tomorrow</span>
-						</h1>
-
-						<p className="font-normal">
-							{"- "}
-							<span className="font-bold">Affordable</span> Personal Properties
-							Options <span className="font-bold text-orange-400">Today!</span>
-						</p>
-
-						<p>
-							Here you can discover our humble listing of property scuh as
-							houses and lands spread across Indonesia
-						</p>
-
-						<Link
-							to="/properties"
-							className="flex items-center justify-center w-32 gap-2 px-4 py-2 text-white bg-orange-400 rounded-xl"
-						>
-							our listing <FaArrowRightLong />
-						</Link>
-					</div>
+					<HeroText />
 
 					<HeroImages />
 				</div>
---
+++ src/pages/Main/Hero/HeroText.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+import { FaArrowRightLong, FaDiamond } from "react-icons/fa6";
+
+export default function HeroText() {
+	return (
+		<div className="absolute z-10 flex flex-col justify-center h-full gap-8 p-6 text-white bg-black sm:justify-normal sm:bg-white sm:w-3/5 sm:text-gray-600 backdrop-blur-sm sm:static bg-opacity-40">
+			<FaDiamond className="text-4xl text-orange-400 animate-pulse" />
+			<h1 className="pb-8 text-4xl border-b">
+				<span className="font-semibold">Invest</span> in your{" "}
+				<span className="font-semibold text-orange-400">Tomorrow</span>
+			</h1>
+
+			<p className="font-normal">
+				{"- "}
+				<span className="font-bold">Affordable</span> Personal Properties
+				Options <span className="font-bold text-orange-400">Today!</span>
+			</p>
+
+			<p>
+				Here you can discover our humble listing of property scuh as houses and
+				lands spread across Indonesia
+			</p>
+
+			<div className="flex gap-2">
+				<Link
+					to="/properties"
+					className="flex items-center justify-center w-32 gap-2 px-4 py-2 text-white bg-orange-400 rounded-xl"
+				>
+					our listing <FaArrowRightLong />
+				</Link>
+				<button className="flex items-center justify-center w-32 gap-2 px-4 py-2 text-white bg-gray-700 rounded-xl">
+					contact us
+				</button>
+			</div>
+		</div>
+	);
+}
